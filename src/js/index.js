482efaa09148dc9e722aa3ad146157d7c12d3899
Stephane Cottereau <scottereau.eurostaff@tf1.fr>
2019-12-13 21:44:16 +0100

    Remove stylelint webpack plugin triggering full page reload on save

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import { hot } from 'react-hot-loader';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import '@babel/polyfill';
 import App from './components/App';
 
 import '../scss/main';
